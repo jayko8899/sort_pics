8641c1cf71510008e371bb856cf0222ac2e76046
jayko8899 <jjarleth99@gmail.com>
2022-02-20 00:26:48 -0500

    improvements to code and readme

--- README.txt
+++ README.txt
@@ -1,6 +1,15 @@
-Python project that will organize pictures by data.
+Python project that will organize pictures by date.
 
 TO RUN:
 
-  1: Modify source folder variable to point to folder containing unsorted media files
-  2: Modify destination folder to point to the folder where media files will be stored
+2 required positional arguments: source folder and destination folder
+
+Source folder is where the program will look for unsorted media files
+
+Destination folder is where the program will store media files once they are sorted
+
+1 optional flag: -d
+
+If -d flag set then program will delete all media files in the source folder once it is done copying them to the destination folder 
+
+Example: "python3 media_sort.py ./photos ./sorted_photos"     #unsorted files are in ./photos and will be sorted into ./sorted_photos
--- media_sort.py
+++ media_sort.py
@@ -1,16 +1,20 @@
-#print("Hello world")
-import os, time
+import os, argparse
 from PIL import Image
 from PIL.ExifTags import TAGS
 import shutil
 from datetime import date
 import hashlib
 
+#acceptable file types
+FTYPES = {"jpg", "JPG", "JPEG", "THM", "PNG", "png"}
+
+TAG_NAME = "DateTimeOriginal"
+
 #function takes the name of an image and returns date it was taken yyyy:mm:dd
 #if file fails to open "Invalid File" is returned
-def get_date( file_name ):
+def get_date(path):
 
-    image = Image.open(image_folder_path + '/' + file_name)
+    image = Image.open(path)
     
     # extract EXIF data
     exifdata = image.getexif()
@@ -19,98 +23,108 @@ def get_date( file_name ):
     for tag_id in exifdata:
         # get the tag name, instead of human unreadable tag id
         tag = TAGS.get(tag_id, tag_id)
-        if tag == tag_name:
+        if tag == TAG_NAME:
             data = exifdata.get(tag_id)
             return data[0:10].replace(":", "_")
 
     #did not have date created field in exif data so we will use modified date
-    timestamp = os.path.getmtime(image_folder_path + '/' + file_name)
+    timestamp = os.path.getmtime(path)
     dt_object = date.fromtimestamp(timestamp)
     m_date = dt_object.isoformat()
     m_date = m_date.replace("-", "_")
     return m_date
 
 #function deletes all files in image folder ignoring directories
-def clear_files():
-    f_list = os.listdir(image_folder_path)
+def clear_files(fpath):
+    f_list = os.listdir(fpath)
     for name in f_list:
-        if os.path.isdir(image_folder_path + '/' + name):
+        if os.path.isdir(fpath + '/' + name):
             continue
         else:
-             os.remove(image_folder_path + '/' + name)
+             os.remove(fpath + '/' + name)
 
-#acceptable file types
-ftypes = {"jpg", "JPG", "JPEG", "THM", "PNG", "png"}
-#tag name to be extracted
-tag_name = "DateTimeOriginal"
-#src folder
-image_folder_path = "/home/jja/JJA/temp/Photos_Sort"
-file_list = os.listdir(image_folder_path)
+def main():
 
-for fn in file_list:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("src_folder", help="Source folder to read raw media files from")
+    parser.add_argument("dest_folder", help="Destination folder to store media files in once sorted")
+    parser.add_argument("-d", '--delete', help="If set old media files will be deleted after they have been sorted", action = "store_true", required=False)
 
+    args = parser.parse_args()
 
-    if os.path.isfile(image_folder_path + '/' + fn):
+    src_image_folder_path = args.src_folder
+    dest_image_folder_path = args.dest_folder
+    file_list = os.listdir(src_image_folder_path)
 
-        #grabbing the extension of the file
-        ext = fn.split('.',1)[1]
-        #seeing if current file is an acceptable type
-        if ext not in ftypes:   
-            continue
+    for fn in file_list:
 
-        #getting the date from the file    
-        date_result = get_date(fn)
-        
-        if date_result == "Invalid File":
-            print("Invalid File: ", fn)
-            continue
+        src_file_path = src_image_folder_path + '/' + fn
 
-        print(fn, " ", date_result)
+        if os.path.isfile(src_file_path):
 
-        dest = image_folder_path + '/' + date_result
+            #grabbing the extension of the file
+            ext = fn.split('.',1)[1]
+            #seeing if current file is an acceptable type
+            if ext not in FTYPES:   
+                continue
 
-        #if date directory already exists we want to copy new file to it
-        #if not we want to create it
-        if os.path.exists(dest):
+            #getting the date from the file    
+            date_result = get_date(src_file_path)
+            
+            if date_result == "Invalid File":
+                print("Invalid File: ", fn)
+                continue
 
-            #checking if file already exists
-            if os.path.exists(dest + '/' + fn):
+            print(fn, " ", date_result)
 
-                #generating md5 hashes
-                old_file = open(dest + '/' + fn, 'rb')
-                new_file = open(image_folder_path + '/' + fn, 'rb')
-                old_file_read = old_file.read()
-                new_file_read = new_file.read()
-                
-                old_md5_hash = hashlib.md5()
-                new_md5_hash = hashlib.md5()
+            dest = dest_image_folder_path + '/' + date_result
 
-                old_md5_hash.update(old_file_read)
-                new_md5_hash.update(new_file_read)
+            #if date directory already exists we want to copy new file to it
+            #if not we want to create it
+            if os.path.exists(dest):
 
-                old_check = old_md5_hash.hexdigest()
-                new_check = new_md5_hash.hexdigest()
+                #checking if file already exists
+                if os.path.exists(dest + '/' + fn):
 
-                #checking if files are the same
-                if old_check == new_check:
-                    pass
+                    #generating md5 hashes
+                    old_file = open(dest + '/' + fn, 'rb')
+                    new_file = open(src_file_path, 'rb')
+                    old_file_read = old_file.read()
+                    new_file_read = new_file.read()
+                    
+                    old_md5_hash = hashlib.md5()
+                    new_md5_hash = hashlib.md5()
+
+                    old_md5_hash.update(old_file_read)
+                    new_md5_hash.update(new_file_read)
+
+                    old_check = old_md5_hash.hexdigest()
+                    new_check = new_md5_hash.hexdigest()
+
+                    #checking if files are the same
+                    if old_check == new_check:
+                        pass
+                    else:
+
+                        #prompt user for new file name then rename new file and copy to dest folder
+                        rename_prompt = f"Name conflict of unequal files: {fn} \nPlease enter new file name:\n"
+                        new_name = input(rename_prompt)
+                        os.rename(src_file_path, src_image_folder_path + '/' + new_name)
+                        shutil.copy2(src_image_folder_path + '/' + new_name, dest)
                 else:
 
-                    #prompt user for new file name then rename new file and copy to dest folder
-                    rename_prompt = f"Name conflict of unequal files: {fn} \nPlease enter new file name:\n"
-                    new_name = input(rename_prompt)
-                    os.rename(image_folder_path + '/' + fn, image_folder_path + '/' + new_name)
-                    shutil.copy2(image_folder_path + '/' + new_name, dest)
+                    #no file conflict, we just copy
+                    shutil.copy2(src_file_path, dest)
             else:
 
-                #no file conflict, we just copy
-                shutil.copy2(image_folder_path + '/' + fn, dest)
+                #directory didnt exist, create directory and copy file to it
+                os.mkdir(dest)
+                shutil.copy2(src_file_path, dest)
         else:
+            continue
+    
+    if args.delete:
+        clear_files(src_image_folder_path)
 
-            #directory didnt exist, create directory and copy file to it
-            os.mkdir(dest)
-            shutil.copy2(image_folder_path + '/' + fn, dest)
-    else:
-        continue
-
-clear_files()
+if __name__ == "__main__":
+    main()
